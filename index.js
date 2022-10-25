1b4b4fed1f6d81662f78e454780f827373ce0364
Vadym <draykavg@gmail.com>
2022-10-25 10:57:24 +0300

    make generator for client secret

---
+++ index.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+const fs = require('fs');
+const path = require('path');
+require('dotenv').config();
+
+const generateAppleToken = () => {
+    const clientId = process.env.APPLE_CLIENT_ID;
+    const teamId = process.env.APPLE_TEAM_ID;
+    const keyId = process.env.APPLE_KEY_ID;
+    const p8Path = process.env.APPLE_P8_PATH;
+    const privateKey = fs.readFileSync(path.join(__dirname, p8Path)).toString();
+
+
+    const now = new Date()
+    const expire = new Date(now);
+    expire.setFullYear(now.getFullYear() + 1);
+    console.log(jwt.sign({}, privateKey, {
+        algorithm: "ES256",
+        expiresIn: "1y",
+        audience: "https://appleid.apple.com",
+        subject: clientId,
+        issuer: teamId,
+        keyid: keyId,
+    }));
+
+}
+
+generateAppleToken()
